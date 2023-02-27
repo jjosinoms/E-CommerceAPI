0cbb7fbedee01e971838d20a224829a1263ca723
Jonas <jjosinoms@gmail.com>
2023-02-27 12:42:06 -0300

    Organizacao do projeto

---
+++ ApiProdutos.Data/Entities/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProdutos.Data.Entities
+{
+    public class Categoria
+    {
+        private Guid? _idCategoria;
+        private string? _nome;
+        private string? _descricao;
+
+        public Guid? IdCategoria { get => _idCategoria; set => _idCategoria = value; }
+        public string? Nome { get => _nome; set => _nome = value; }
+        public string? Descricao { get => _descricao; set => _descricao = value; }
+    }
+}
