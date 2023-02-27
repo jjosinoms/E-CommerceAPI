64aaeab2223b136626e821ba687ddcfdd70d63b2
Jonas <jjosinoms@gmail.com>
2023-02-27 11:39:49 -0300

    Organizando API de produtos e categorias para o Projeto E-Commerce

---
+++ ApiProdutos.Services/Models/ProdutosGetModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ApiProdutos.Services.Models
+{
+    public class ProdutosGetModel
+    {
+        public Guid? IdProduto { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal? Preco { get; set; }
+        public int? Quantidade { get; set; }
+        public DateTime DataHoraCadastro { get; set; }
+
+        /// <summary>
+        /// Dados da categoria do produto
+        /// </summary>
+        public CategoriasGetModel? Categoria { get; set; }
+    }
+}
---
+++ ApiProdutos.Services/Models/ProdutosPostModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiProdutos.Services.Models
+{
+    public class ProdutosPostModel
+    {
+        [MinLength(6, ErrorMessage = "Informe no mínimo {1} caracteres.")]
+        [MaxLength(50, ErrorMessage = "Informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe o nome do produto.")]
+        public string? Nome { get; set; }
+
+        [MinLength(6, ErrorMessage = "Informe no mínimo {1} caracteres.")]
+        [MaxLength(150, ErrorMessage = "Informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe a descrição do produto.")]
+        public string? Descricao { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a preço do produto.")]
+        public decimal? Preco { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a quantidade do produto.")]
+        public int? Quantidade { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o id da categoria.")]
+        public Guid? IdCategoria { get; set; }
+
+
+    }
+}
